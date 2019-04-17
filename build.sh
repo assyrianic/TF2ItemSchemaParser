8387a879798f3d3809ae1e602d3d03fa4de7d357
Kevin Yonan <assyrianic@users.noreply.github.com>
2019-04-17 10:55:09 -0700

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-gcc -Wall -Wextra -Woverflow -std=c99 -g -Os stringobj.c kvtree.c main.c -o TF2ItemSchemaParser
-#clang-3.5 -std=c11 -g -O3 stringobj.c list.c dict.c main.c -o compiler
-#don't forget -g for debugging
-
-#valgrind --leak-check=full --show-leak-kinds=all ./SonaParser testparse.c
-#gcc -std=c11 -O3 -S -masm=intel symbol.c
+gcc -Wall -Wextra -std=c99 -s -O2 stringobj.c kvtree.c main.c -o TF2ItemSchemaParser
